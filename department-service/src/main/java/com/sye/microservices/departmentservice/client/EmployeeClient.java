8260472af43eeda485baac7a936e868bcbc8d725
syeluri <syeluri@atd-us.com>
2019-01-04 11:21:33 -0500

    Init Checkin
    Init Checkin

---
+++ department-service/src/main/java/com/sye/microservices/departmentservice/client/EmployeeClient.java
@@ -0,0 +1,17 @@
+package com.sye.microservices.departmentservice.client;
+
+import java.util.List;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.sye.microservices.departmentservice.domain.Employee;
+
+@FeignClient(name = "employee-service")
+public interface EmployeeClient {
+
+	@GetMapping("/department/{departmentId}")
+	List<Employee> findByDepartment(@PathVariable("departmentId") Long departmentId);
+	
+}
---
+++ organization-service/src/main/java/com/sye/microservices/organizationservice/client/DepartmentClient.java
@@ -0,0 +1,20 @@
+package com.sye.microservices.organizationservice.client;
+
+import java.util.List;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.sye.microservices.organizationservice.domain.Department;
+
+@FeignClient(name = "department-service")
+public interface DepartmentClient {
+
+	@GetMapping("/organization/{organizationId}")
+	public List<Department> findByOrganization(@PathVariable("organizationId") Long organizationId);
+	
+	@GetMapping("/organization/{organizationId}/with-employees")
+	public List<Department> findByOrganizationWithEmployees(@PathVariable("organizationId") Long organizationId);
+	
+}
